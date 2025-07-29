dc0ca95980f993e7afc25e8a2ff86922f4aea285
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-07-29 17:26:48 +0200

    [feature] add test discovery settings for src directory

--- src/rest_weather/settings.py
+++ src/rest_weather/settings.py
@@ -162,3 +162,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+TEST_DISCOVER_TOP_LEVEL = BASE_DIR / "src"
+TEST_DISCOVER_ROOT = BASE_DIR / "src" / "apps"
